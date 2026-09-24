f1c1a579c2c6808e70511194c24df996ee7cbd9a
Paula Rodrigues <rodriguespereirapaula649@gmail.com>
2026-09-24 08:39:59 -0300

    Add files via upload

---
+++ Exercicio-2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Digite o primeiro número: ");
+        int num1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Digite o segundo número: ");
+        int num2 = int.Parse(Console.ReadLine());
+
+        bool eMenor = num1 < num2;
+        Console.WriteLine(" O primeiro número e menor que o segundo: " + eMenor);
+    }
+}
