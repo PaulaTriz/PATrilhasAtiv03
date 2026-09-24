8527854bc6fd04f8f8b5be93f99a79bac8c8e73a
Paula Rodrigues <rodriguespereirapaula649@gmail.com>
2026-09-24 08:53:58 -0300

    Add files via upload

---
+++ Exercicio-3.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Digite o primeiro número: ");
+        int num1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Digite o segundo número: ");
+        int num2 = int.Parse(Console.ReadLine());
+
+        bool saoIguais = num1 == num2;
+        Console.WriteLine(" O primeiro número e igual o segundo: " + saoIguais);
+    }
+}
