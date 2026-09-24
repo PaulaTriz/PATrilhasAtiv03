552b862acd650c5bb33400f3b0e9a9bcbf4de445
Paula Rodrigues <rodriguespereirapaula649@gmail.com>
2026-09-24 10:02:24 -0300

    Add files via upload

---
+++ Exercicio-8.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Digite um número inteiro: ");
+        int num = int.Parse(Console.ReadLine());
+
+        if (num >= 0)
+        {
+            Console.WriteLine("O número e positivo: ");
+        }
+        else
+        {
+            Console.WriteLine("O número e negativo: ");
+        }
+    }
+}
