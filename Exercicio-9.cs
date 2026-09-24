23fc26eecf86a001b9cd3dd7a2d896aec312c69a
Paula Rodrigues <rodriguespereirapaula649@gmail.com>
2026-09-24 10:27:58 -0300

    Add files via upload

---
+++ Exercicio-9.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Digite o primeiro número inteiro: ");
+        int num1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Digite o segundo número inteiro: ");
+        int num2 = int.Parse(Console.ReadLine());
+
+        int diferenca = Math.Abs(num1 - num2);
+
+        if (diferenca <= 10)
+        {
+            Console.WriteLine("A diferenca entre dois números é: " + diferenca + " ( Menor ou igual a 10).");
+        } else
+        {
+            Console.WriteLine("A diferenca entre dois números é: " + diferenca + " (Maior ou igual a 10).");
+        }
+    }
+}
