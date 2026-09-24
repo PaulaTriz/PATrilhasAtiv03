5a66b29131e3b6623bc297766ef1f2609d3ac8f9
Paula Rodrigues <rodriguespereirapaula649@gmail.com>
2026-09-24 10:36:44 -0300

    Add files via upload

---
+++ Exercicio-10.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Digite um número flutuante: ");
+        float num = float.Parse(Console.ReadLine());
+
+        bool flutuante = num == 0;
+        Console.WriteLine("0 número flutuante e igual a zero: " + flutuante);
+    }
+}
