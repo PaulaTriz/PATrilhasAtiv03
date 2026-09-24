3e5e9bf9bb8a9d4bcbae4bfeb964c3c80ea85a31
Paula Rodrigues <rodriguespereirapaula649@gmail.com>
2026-09-24 09:06:37 -0300

    Add files via upload

---
+++ Exercicio-4.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Digite o primeiro número: ");
+        int num1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Digite o segundo número: ");
+        int num2 = int.Parse(Console.ReadLine());
+
+        Console.Write("Digite o terceiro número: ");
+        int num3 = int.Parse(Console.ReadLine());
+
+        bool condicao = (num1 > num2) && (num2 > num3);
+        Console.WriteLine("o primerio e menor que segundo e maior que o terceiro: " + condicao);
+    }
+}
