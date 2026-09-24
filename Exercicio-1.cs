bedeb8b2fc3be306dbb04fd854fd67218474077e
Paula Rodrigues <rodriguespereirapaula649@gmail.com>
2026-09-24 08:34:27 -0300

    Add files via upload

---
+++ Exercicio-1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Digite o primeiro número inteiro: ");
+        int num1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Digite o segundo número inteiro: ");
+        int num2 = int.Parse(Console.ReadLine());
+
+        bool eMaior = num1 > num2;
+        Console.WriteLine("O primeiro número e maior que o segundo? " + eMaior);
+    }
+}
