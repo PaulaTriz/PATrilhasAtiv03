03119f65466b24bb837780dde4d3aafc6c0fc4b3
Paula Rodrigues <rodriguespereirapaula649@gmail.com>
2026-09-24 09:32:31 -0300

    Add files via upload

---
+++ Exercicio-6.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Digite um número inteiro: ");
+        int num = int.Parse(Console.ReadLine());
+
+        if (num % 2 == 0)
+        {
+            Console.WriteLine(" O número e par: ");
+        } else
+        {
+            Console.WriteLine("O número e impar: ");
+        }
+
+    }
+}
